887dc6fa175c72d689f1ae64ed72c0cf076c958f
FAHIMKABIRCHOWDHURY <74867164+FAHIMKABIRCHOWDHURY@users.noreply.github.com>
2022-11-08 16:40:36 +0600

    Add files via upload

---
+++ springWebmvcHibernateValidator/src/main/java/com/controller/RegistrationController.java
@@ -0,0 +1,49 @@
+package com.controller;
+
+import com.domain.User;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.sql.DataSource;
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/registration")
+public class RegistrationController {
+
+    private DataSource dataSource;
+
+    public RegistrationController(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @InitBinder
+    public void intiBinder(WebDataBinder webDataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
+    @RequestMapping("/form")
+    public String form(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "registration";
+    }
+
+    @RequestMapping("/submit")
+    public String form(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+
+        model.addAttribute("message", "Successful");
+        return "confirmation";
+
+    }
+}
---
+++ springWebmvcHibernateValidator/src/main/webapp/WEB-INF/views/confirmation.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kawsu
+  Date: 10/25/2022
+  Time: 8:56 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page isELIgnored="false" %>
+<html>
+<head>
+    <title>Confiramtion</title>
+</head>
+<body>
+<h1>Confirm</h1>
+<c:out value="${message}" />
+<p>Welcome, ${user.email} ${user.password}</p>
+</body>
+</html>
